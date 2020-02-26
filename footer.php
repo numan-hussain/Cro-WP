4103acb1f9360e2a4c3858a0fc3f27ae078bbc49
Numan Hussain <ahnuman5@gmail.com>
2020-02-26 23:50:40 +0600

    added footer codes

---
+++ footer.php
@@ -0,0 +1,111 @@
+
+	<!--================================
+			Section Nine: conact
+		==============================-->
+        <footer>
+		<div class="container">
+			<div class="row">
+
+				<!-- first column -->
+				<div class="col-sm-3 col-xs-6">
+
+					<!-- column heading -->
+					<h5>product</h5>
+
+					<!-- column items -->
+					<ul class="list-unstyled">
+						<li><a href="#">popular</a></li>
+						<li><a href="#">trending</a></li>
+						<li><a href="#">catalog</a></li>
+						<li><a href="#">feature</a></li>
+					</ul>
+					<!-- .end/ column items -->
+
+				</div>
+				<!-- .end/ first column -->
+
+				<!-- second column -->
+				<div class="col-sm-3 col-xs-6">
+
+					<!-- column heading -->
+					<h5>info</h5>
+
+					<!-- column items -->
+					<ul class="list-unstyled">
+						<li><a href="#">support</a></li>
+						<li><a href="#">developer</a></li>
+						<li><a href="#">service</a></li>
+						<li><a href="#">get started</a></li>
+					</ul>
+					<!-- .end/ column items -->
+
+				</div>
+				<!-- .end/ second column -->
+
+				<!-- third column -->
+				<div class="col-sm-3 col-xs-6">
+
+					<!-- column heading -->
+					<h5>company</h5>
+
+					<!-- column items -->
+					<ul class="list-unstyled">
+						<li><a href="#">press release</a></li>
+						<li><a href="#">mission</a></li>
+						<li><a href="#">strategy</a></li>
+						<li><a href="#">work</a></li>
+					</ul>
+					<!-- .end/ column items -->
+
+				</div>
+				<!-- .end/ third column -->
+
+				<!-- fourth column -->
+				<div class="col-sm-3 col-xs-6">
+
+					<!-- column heading -->
+					<h5>contact</h5>
+
+					<!-- column items -->
+					<ul class="list-unstyled">
+						<li><a href="#">popular</a></li>
+						<li><a href="#">trending</a></li>
+						<li><a href="#">catalog</a></li>
+						<li><a href="#">feature</a></li>
+					</ul>
+					<!-- .end/ column items -->
+
+				</div>
+				<!-- .end/ fourth column -->
+
+			</div>
+
+			<!-- copyright message -->
+			<div class="row">
+				<div class="text-center">
+					<p>&copy 2016 Landing. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- .end/ footer -->
+
+
+	<!--================
+				SCRIPT
+		====================-->
+
+	<!-- JQUERY -->
+	<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+
+	<!-- BOOTSTRAP.JS -->
+	<script src="assets/js/bootstrap.js"></script>
+
+	<!-- BOOTSTRAP.JS -->
+	<script src="assets/js/script.js"></script>
+
+
+	
+	<?php wp_footer();?>
+</body>
+</html>
